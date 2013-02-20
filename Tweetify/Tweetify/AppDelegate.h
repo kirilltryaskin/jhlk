ae925bde1b1efc4d8551621cce395204f320a3bc
smr <smr@marseil.local>
2013-02-20 15:37:54 +0200

    empty project

---
+++ Tweetify/Tweetify/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Tweetify
+//
+//  Created by smr on 20.02.13.
+//  Copyright (c) 2013 smr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
